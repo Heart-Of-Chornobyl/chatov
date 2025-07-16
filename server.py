7ff186632ad99ef13cb10b5266af76160323541a
Heart-Of-Chornobyl <vladislav.lisickiy@gmail.com>
2025-07-16 21:47:11 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
-import os
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
-def hello():
-    return "Привет, мир!"
+def index():
+    return "Сервер чата работает!"
+
+@socketio.on('message')
+def handle_message(msg):
+    print(f"Получено сообщение: {msg}")
+    send(msg, broadcast=True)  # Отправляем всем подключённым
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=8000)
