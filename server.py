1cc485f671055f669cd16b9eac60972268ba0aa0
Heart-Of-Chornobyl <vladislav.lisickiy@gmail.com>
2025-07-16 21:55:27 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
@@ -12,7 +12,7 @@ def index():
 @socketio.on('message')
 def handle_message(msg):
     print(f"Получено сообщение: {msg}")
-    send(msg, broadcast=True)  # Отправляем всем подключённым
-    
+    send(msg, broadcast=True)
+
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
