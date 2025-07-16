0a3c365f0194b31afc65e437667c77e8cb920827
Heart-Of-Chornobyl <vladislav.lisickiy@gmail.com>
2025-07-16 21:53:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -13,6 +13,6 @@ def index():
 def handle_message(msg):
     print(f"Получено сообщение: {msg}")
     send(msg, broadcast=True)  # Отправляем всем подключённым
-
+    
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000)
+    socketio.run(app, host='0.0.0.0', port=8000, allow_unsafe_werkzeug=True)
