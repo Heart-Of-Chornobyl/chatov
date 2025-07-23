602bfb00a5bfc6eaedaee0dde0b92d24bc24e129
Heart-Of-Chornobyl <vladislav.lisickiy@gmail.com>
2025-07-23 19:25:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
+import asyncio
+from telegram.ext import Application, CommandHandler
 import os
 
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -6,12 +7,10 @@ BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 async def start(update, context):
     await update.message.reply_text("Привет! Бот работает.")
 
-def main():
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-
+async def main():
+    app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-
-    app.run_polling()
+    await app.run_polling()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
