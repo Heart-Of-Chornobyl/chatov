1d29caf83f8727e7360068be426172ebcad2fd81
Heart-Of-Chornobyl <vladislav.lisickiy@gmail.com>
2025-07-23 19:10:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привет!")
 
 if __name__ == "__main__":
-    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()  # <-- исправлено
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     app.run_polling()
